14c1ae7737f84cb3c2a224adc97948b0fb84241d
Sam McDonald <s.mcdonald@outlook.com.au>
2021-10-23 13:51:43 +1000

    Added update to the RSP  to bind route to Movie
    Core work in getting the Movie Model binded to the route, added to Movie controller

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Movie;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Http;
+
+/**
+ * MovieController
+ * 
+ * The MovieController is responsible for tasks
+ * related to a specific Movie. Such as
+ * CRUD actions typically.
+ */
+class MovieController extends BaseController
+{
+    /**
+     * index
+     *
+     * This controller using DI and also includes
+     * a request object. Thgis object is
+     * checking for a max search string.
+     *
+     * @param  mixed $request
+     * @return void
+     */
+    public function index(Request $request, Movie $movie)
+    {
+        $items = $request->validate([
+            'p' => 'sometimes|min:1|max:50'
+        ]);
+
+        $phrase = Str::replace(" ","%20%", $items['p']);
+
+        return view('movie', compact('movie','phrase'));
+    }
+}
---
+++ app/Models/Movie.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Support\Facades\Http;
+
+class Movie 
+{
+    protected $movie_title;
+    
+    /**
+     * __construct
+     *
+     * @param  mixed $response
+     * @return void
+     */
+    public function __construct($response)
+    {
+        $this->year = $response->Year;
+        $this->title = $response->Title;
+        $this->rated = $response->Rated;
+        $this->genre = $response->Genre;
+        $this->plot = $response->Plot;
+        $this->imdb = $response->imdbID;
+        $this->poster = $response->Poster;
+        $this->runtime = $response->Runtime;
+    }
+    
+    /**
+     * fetch
+     *
+     * Call external api and decode posiotive result.
+     * 
+     * @todo - Implement alternate identification methods.
+     * @param  mixed $field The field to search for
+     * @param  mixed $value If $field is imdb then value is the imdb id
+     * @return Movie or NULL
+     */
+    public static function fetch($field, $value) : ?Movie
+    {
+        $userkey = env('OMDBAPI_API_KEY');
+
+        $url = "http://www.omdbapi.com/?apikey={$userkey}&i={$value}&type=movie&plot=full&r=json";
+        $response = Http::get($url);
+
+        if ($response->successful()) {
+            $movie_obj = json_decode($response->body());
+            return new self($movie_obj);
+        }
+
+        return NULL;
+    }
+    
+    /**
+     * findByPhrase
+     *
+     * @param  mixed $phrase
+     * @param  mixed $page
+     * @return void
+     */
+    public static function findByPhrase($phrase = "", int $page = 1)
+    {
+        $results = [];
+        $userkey = env('OMDBAPI_API_KEY');
+        $url = "http://www.omdbapi.com/?apikey={$userkey}&s={$phrase}&page={$page}&movie=movie&v=1";
+        $response = Http::get($url);
+
+        if ($response->successful()) {
+            $results = $response->body();
+        }
+
+        return json_decode($results);
+    }
+}
