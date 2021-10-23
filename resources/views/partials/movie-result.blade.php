3e01b8c30f1cff3605371e099c77961020c99863
Sam McDonald <s.mcdonald@outlook.com.au>
2021-10-23 13:27:58 +1000

    Update styles for 5x grid, Pt2
    Forgot to add these two files in last commit

--- resources/views/partials/movie-result.blade.php
+++ resources/views/partials/movie-result.blade.php
@@ -1,6 +1,9 @@
 <div class="result-item">
-    <h2>Back to the Future</h2>
-    <span class="rating">8.5/10</span>
-    <p>Marty McFly, a 17-year-old high school student, is accidentally sent thirty years into the past in a time-traveling DeLorean invented by his close friend, the eccentric scientist Doc Brown.</p>
-    <a href="{{ url('/view-movie') }}" class='btn'>View</a>
+    <img src="{{ $movie->Poster }}" alt="{{ $movie->Title }} Poster">
+    <h2>
+        <a href="{{ route('movie', ['movie' => $movie->imdbID,  'p' => $phrase]) }}">
+            {{ $movie->Title }}
+            <span class="year">({{ $movie->Year }})</span>
+        </a>
+    </h2>
 </div>
