8d12c15f37035689b32693eb14c9a2cac8042d41
Sam McDonald <s.mcdonald@outlook.com.au>
2021-10-23 13:04:38 +1000

    Update the VIEW  for the search bar
    update the search bar VIEW to post to search route and other ui adjustments

--- resources/views/partials/big-search.blade.php
+++ resources/views/partials/big-search.blade.php
@@ -1,17 +1,17 @@
 <div class="p-5 mb-4 bg-light rounded-3">
     <div class="container-fluid py-5">
         <h1 class="display-5 fw-bold">
-            Search 
-            @include('icons.imdb-icon')
-            now
+            Search movie titles now
         </h1>
         <p class="col-md-8 fs-4">
-            Why use imdb when you can use this app! It has all your favourite movies, tv series and more
-            Its as simple as 1..2.3, just enter the movie you would like to get data on
+            Search the online dtabase of movies here.
         </p>
         <div class="search-box-wrapper">
-            <input type="text" class="form-control movie-search-bar">
-            <button class="btn custom-button" type="button">Search</button>
+            <form method="POST" action="{{ route('search') }}">
+                @csrf
+                <input name="s" type="text" class="form-control movie-search-bar" value="{{ $phrase }}">
+                <button class="btn custom-button" type="submit">Search</button>
+            </form>
         </div>
     </div>
 </div>
